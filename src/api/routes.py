61896e5c45a6c424fdece6065a64b7b513cabfbd
Lulu828-cyber <146968254+Lulu828-cyber@users.noreply.github.com>
2024-05-26 10:08:09 +0000

    finally

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,10 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_cors import CORS 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 from flask_cors import CORS
 
 api = Blueprint('api', __name__)
@@ -12,66 +16,35 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
-
-#### USER LOG IN
-
-@api.route("/login", methods=["POST"])
-def login():
-
-    email = request.json.get("email")
-    password = request.json.get("password")
-
-    user = User.query.filter_by(email=email, password=password).first()
-    if not user:
-        return jsonify("Credenciales incorrectas"), 401
-
-    access_token = create_access_token(identity=email)
-
-    response_body = {
-        "msg": "logged",
-        "user": user.serialize(),
-        "token": access_token
-    }
-    print(response_body),
-    return jsonify(response_body), 200
-
-### Sign Up ##
-
-@api.route("/signup", methods=["PORST"])
-def register():
-
-    request_body = request.get_jsn(force=True)
-
-    required_fields -["email", "password"]
-    for field in required_fields:
-        if field not in request_body or not request_body[field]:
-            raise APIException('The field cannot be empty', 400)
-        
-        verify_email = User.query.filter_by(email=request_body).first()
-        if verify_email:
-            raise APIException('An account already exit with this email', 400)
-        
-         user = User(email=request_body["email"], password=request_body["password"])
-
+@api.route('/signup', methods=['POST'])
+def handle_signup():
+    request_body = request.get_json()
+   
+    email = request_body.get('email')
+    password = request_body.get('password')
+  
+    user = User(email=email, password=password)
     db.session.add(user)
-    print(user)
-
-    try:
-        db.session.commit()
-    except:
-        raise APIException('Internal error', 500)
-
-    response_body = {
-        "msg": "Successfull! new user created.",
-        "user": user.serialize()
+    db.session.commit()
+    reponse_body = {
+        'msg': 'User created successfully',
+        'user': user.serialize()
     }
 
-    return jsonify(response_body), 200
+
+    return jsonify(reponse_body), 200
+
+
+ 
+
+@api.route('/login', methods=['POST'])
+def handle_login():
+    request_body = request.get_json()
+    email = request_body.get('email')
+    password = request_body.get('password')
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        return jsonify({'msg': 'Error en el email o password'}), 401
+   
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token), 200
---
+++ src/front/js/pages/login.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import getState from "../store/flux";
+
+export const Login = () => {
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    actions.getLogin(loginEmail, loginPassword);
+  };
+
+  const { store, actions } = useContext(Context);
+  useEffect(() => {
+    actions.getLogin();
+  }, []);
+
+ /*    useEffect(() => {
+    if (store.token !== null) {
+      navigate("/private");
+    }
+  }, [store.token, navigate]); */
+
+  console.log(navigate);
+  console.log(store);
+  return (
+    <div className="text-center mt-5">
+      <h1>login</h1>
+      <p> You need to login to access the content</p>
+
+      <form>
+        <div className="container">
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              value={store.email}
+              onChange={(e) => setLoginEmail(e.target.value)}
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              value={store.password}
+              onChange={(e) => setLoginPassword(e.target.value)}
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+        </div>
+      </form>
+
+      <button
+        onClick={() => {
+          handleLogin();
+          navigate("/private");
+        }}
+      >
+        LOGIN
+      </button>
+    </div>
+  );
+};
